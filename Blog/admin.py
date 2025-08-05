d04b087d83152fcee5bb879755443a1aff365875
Anupamkush9 <anupam.kush9@gmail.com>
2025-08-05 23:55:35 +0530

    actions implemented in Todo and Blog model

--- Blog/admin.py
+++ Blog/admin.py
@@ -3,12 +3,26 @@ from django.contrib import admin
 # Register your models here.
 from .models import Blog_table
 from django.conf import settings
-
+from django.contrib import messages
 
 # admin.site.register(Blog_table)
 @admin.register(Blog_table)
 class Blog_tableAdmin(admin.ModelAdmin):
     list_display = ['id','user_id','title','image','date']
+    actions = ['convert_title_to_title_case']
+
+    def convert_title_to_title_case(self, request, queryset):
+        updated_count = 0
+        for blog in queryset:
+            new_title = blog.title.title()
+            if blog.title != new_title:
+                blog.title = new_title
+                blog.save()
+                updated_count += 1
+        self.message_user(request, f"{updated_count} blog(s) updated to Title Case.", level=messages.SUCCESS)
+
+    convert_title_to_title_case.short_description = "Convert title to Title Case"  # label in dropdown
+
     
     def get_queryset(self, request):
         # Get the default queryset
--- Todo/admin.py
+++ Todo/admin.py
@@ -4,3 +4,14 @@ from .models import Task
 @admin.register(Task)
 class Taskadmin(admin.ModelAdmin):
     list_display = ['user_id','id','title','complete','time']
+    actions = ['mark_as_complete', 'mark_as_incomplete']
+
+    @admin.action(description="Mark selected tasks as complete")
+    def mark_as_complete(self, request, queryset):
+        updated = queryset.update(complete=True)
+        self.message_user(request, f"{updated} task(s) marked as complete.")
+
+    @admin.action(description="Mark selected tasks as incomplete")
+    def mark_as_incomplete(self, request, queryset):
+        updated = queryset.update(complete=False)
+        self.message_user(request, f"{updated} task(s) marked as incomplete.")
